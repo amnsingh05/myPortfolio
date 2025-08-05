00bf2ae50ac19659dc5963b73a6f523073476cbf
amnsingh05 <amansinghakr@gmail.com>
2025-08-05 21:40:30 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const img = document.getElementById("profile-pic");
+  setTimeout(() => {
+    img.style.transform = "scale(1)";
+  }, 200);
+});
